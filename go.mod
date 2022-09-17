7bbb23ad9d29014b7a7269c90be9f5c93a45f8f0
shzxcv <sh.zxcv00@gmail.com>
2022-09-17 22:13:11 +0900

    add logic

--- go.mod
+++ go.mod
@@ -2,4 +2,16 @@ module github.com/shzxcv/slack-notifier-actions
 
 go 1.19
 
-require github.com/google/go-github/v47 v47.0.0 // indirect
+require (
+	github.com/golang/protobuf v1.5.2 // indirect
+	github.com/google/go-github v17.0.0+incompatible // indirect
+	github.com/google/go-github/v47 v47.0.0 // indirect
+	github.com/google/go-querystring v1.1.0 // indirect
+	github.com/gorilla/websocket v1.5.0 // indirect
+	github.com/kelseyhightower/envconfig v1.4.0 // indirect
+	github.com/slack-go/slack v0.11.3 // indirect
+	golang.org/x/net v0.0.0-20220624214902-1bab6f366d9e // indirect
+	golang.org/x/oauth2 v0.0.0-20220909003341-f21342109be1 // indirect
+	google.golang.org/appengine v1.6.7 // indirect
+	google.golang.org/protobuf v1.28.0 // indirect
+)
--- main.go
+++ main.go
@@ -1,7 +1,72 @@
 package main
 
-import "fmt"
+import (
+	"context"
+	"errors"
+	"fmt"
+	"log"
+
+	"github.com/google/go-github/github"
+	"github.com/kelseyhightower/envconfig"
+	"github.com/slack-go/slack"
+	"golang.org/x/oauth2"
+)
+
+type Env struct {
+	GithubToken        string `envconfig:"GITHUB_TOKEN" required:"true"`
+	SlackBotOauthToken string `envconfig:"SLACK_BOT_OAUTH_TOKEN" required:"true"`
+	SlackChannel       string `envconfig:"SLACK_CHANNEL" required:"false"`
+	SlackUserID        string `envconfig:"SLACK_USER_ID" required:"false"`
+}
+
+func (e *Env) NewEnv() {
+	if err := envconfig.Process("", e); err != nil {
+		log.Fatal(err)
+	}
+}
 
 func main() {
-	fmt.Println("testaaa")
+	var e Env
+	e.NewEnv()
+	err := notifications(&e)
+	fmt.Println(err)
+}
+
+func notifications(e *Env) error {
+	ctx := context.Background()
+	ts := oauth2.StaticTokenSource(&oauth2.Token{AccessToken: e.GithubToken})
+	tc := oauth2.NewClient(ctx, ts)
+	client := github.NewClient(tc)
+	notifications, _, err := client.Activity.ListNotifications(ctx, nil)
+	if err != nil {
+		return err
+	}
+	for _, n := range notifications {
+		subjects := n.GetSubject()
+		fmt.Println(subjects.GetLatestCommentURL())
+	}
+	return nil
+}
+
+func send(text string, e *Env) error {
+	if e.SlackUserID == "" && e.SlackChannel == "" {
+		return errors.New("SlackUserID and SlackChannel is empty")
+	}
+	var channelID string
+	if e.SlackUserID != "" && e.SlackChannel != "" {
+		// Direct Message has priority
+		channelID = e.SlackUserID
+	}
+	if e.SlackUserID == "" && e.SlackChannel != "" {
+		channelID = e.SlackUserID
+	}
+	if e.SlackUserID != "" && e.SlackChannel == "" {
+		channelID = e.SlackUserID
+	}
+	client := slack.New(e.SlackBotOauthToken)
+	_, _, err := client.PostMessage(channelID, slack.MsgOptionText(text, true))
+	if err != nil {
+		return err
+	}
+	return nil
 }
